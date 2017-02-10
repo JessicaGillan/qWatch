a74381dea1bfa69867cb4398385403a291516f6d
Jessica G <gillan.jessica@gmail.com>
2017-02-10 09:20:16 -0700

    fix watchable params

--- app/models/guidebox.rb
+++ app/models/guidebox.rb
@@ -3,33 +3,82 @@
 
 class Guidebox
 
-  class << self
+  # class << self
 
   API_KEY = "?api_key=#{Rails.application.secrets.guidebox_api_key}"
   BASE_URL = 'https://api-public.guidebox.com/v2/'
+  LIMIT_MAX = 250
 
-  def pull_movies
-    response = HTTParty.get(build_url('movies'))
-    JSON.parse(response.body)["results"]
+  def initialize
+    @total_num_movies = nil
+    @num_retrieved = 0
   end
 
-  def movies
-    @movies = pull_movies.each do |movie|
-      watch = Watchable.find_by(gb_id: movie["id"], gb_type: "movie")
-      # if watch
-      #   watch.update(title: movie["title"], {poster_attributes: { thumbnail: movie["poster_120x171"], medium: movie["poster_240x342"], large: movie["poster_400x570"] }})
-      # else
-      #   Watchable.create(gb_id: movie["id"], gb_type: "movie", title: movie["title"], {poster_attributes: { thumbnail: movie["poster_120x171"], medium: movie["poster_240x342"], large: movie["poster_400x570"] }})
-      # end
+  # DO NOT CALL UNLESS INTEND TO UPDATE ENTIRE DATABASE
+  def save_all_movies
+    while @num_retrieved < @total_num_movies
+      save_movies({ offset: @num_retrieved, limit: LIMIT_MAX })
     end
+  end
+
+  def save_movies(options = {})
+    results = Guidebox.pull_movies(options)
+    puts "RESULTS"
+    p results
+
+    @num_retrieved += results.length
+
+    results.each do |movie|
+      watch = Watchable.find_by(gb_id: movie["id"].to_i, gb_type: "movie")
+
+      if watch
+        watch.update(watchable_params(movie, "movie"))
+      else
+        Watchable.create(watchable_params(movie, "movie"))
+      end
+    end
+  end
+
+  def self.pull_movies(options = {})
+    response = HTTParty.get(build_url('movies', options))
+    json = JSON.parse(response.body)
+
+    @total_num_movies = @total_num_movies || json["total_results"]
+
+    json["results"]
   end
 
   private
-
-    def build_url(product, query=nil)
-      "#{BASE_URL}#{product}#{API_KEY}#{query}"
+    def watchable_params(result, type)
+      return {
+        gb_id: result["id"],
+        gb_type: type,
+        title: result["title"],
+        poster_attributes:
+        {
+          thumbnail: result["poster_120x171"],
+          medium: result["poster_240x342"],
+          large: result["poster_400x570"]
+        }
+      }
     end
 
-  end
+    def self.build_url(product, options = nil)
+      "#{BASE_URL}#{product}#{API_KEY}#{build_query_string(options)}"
+    end
+
+    # Format: &limit=10
+    def self.build_query_string(options)
+      return "" if !options || options.empty?
+
+      q_str = ""
+      options.each do |key, value|
+        q_str += "&#{key}=#{value}"
+      end
+
+      q_str
+    end
+
+  # end
 
 end
--- app/models/poster.rb
+++ app/models/poster.rb
@@ -1,3 +1,3 @@
 class Poster < ApplicationRecord
-  belongs_to :watchable
+  belongs_to :watchable, inverse_of: :poster
 end
