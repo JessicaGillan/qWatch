28f6b16d0b11d56ddbd5cb80d44494dbf63a69c6
Sampson Crowley <sampsonsprojects@gmail.com>
2017-02-09 17:28:29 -0700

    movie wrapper debugging

--- app/models/guidebox.rb
+++ app/models/guidebox.rb
@@ -1,20 +1,27 @@
 # Get all movies - /*https://api-public.guidebox.com/v2/movies?api_key=*/
 # Get movie responses - /*https://api-public.guidebox.com/v2/movies/135934?api_key= */
 
-class GuideBox
+class Guidebox
 
   class << self
 
   API_KEY = "?api_key=#{Rails.application.secrets.guidebox_api_key}"
   BASE_URL = 'https://api-public.guidebox.com/v2/'
 
-  def movies
+  def pull_movies
     response = HTTParty.get(build_url('movies'))
-    JSON.parse(response.body)
+    JSON.parse(response.body)["results"]
   end
 
-  def movie_data
-
+  def movies
+    @movies = pull_movies.each do |movie|
+      watch = Watchable.find_by(gb_id: movie["id"], gb_type: "movie")
+      # if watch
+      #   watch.update(title: movie["title"], {poster_attributes: { thumbnail: movie["poster_120x171"], medium: movie["poster_240x342"], large: movie["poster_400x570"] }})
+      # else
+      #   Watchable.create(gb_id: movie["id"], gb_type: "movie", title: movie["title"], {poster_attributes: { thumbnail: movie["poster_120x171"], medium: movie["poster_240x342"], large: movie["poster_400x570"] }})
+      # end
+    end
   end
 
   private
