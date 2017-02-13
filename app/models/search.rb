225731af89955416f0ebf8454fefad5b3138f503
Sampson Crowley <sampsonsprojects@gmail.com>
2017-02-13 10:14:30 -0700

    remove search class

--- app/models/search.rb
+++
@@ -1,9 +0,0 @@
-class Search
-
-  def title(str)
-    conn = ActiveRecord::Base.connection
-    terms = str.split(" ")
-    query = ActiveRecord::Base.send(:sanitize_sql_array, ["SELECT * FROM watchables WHERE title_tsvector @@ to_tsquery('english', ?)", str])
-    # query = conn.execute(query)
-  end
-end
