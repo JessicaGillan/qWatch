158d70c9911563af29aa31645bbd036a54f9ff45
Jessica G <gillan.jessica@gmail.com>
2017-02-13 10:33:22 -0700

    tmdb specs

--- spec/models/tmdb_spec.rb
+++ spec/models/tmdb_spec.rb
@@ -4,8 +4,14 @@ RSpec.describe TMDB, type: :model do
 
   describe '.pull_movies' do
 
-    it 'returns a JSON object'
-    it 'should have response["results"] when it is successful, indicated by response["success"]'
+    it 'returns a JSON object' do
+      expect(TMDB.pull_movies).to be_a Hash
+    end
+
+    it 'should have response["results"] when it is successful, indicated by response["success"]' do
+      expect(TMDB.pull_movies["success"]).to be true
+      expect(TMDB.pull_movies["results"]).to be_truthy
+    end
 
   end
 
