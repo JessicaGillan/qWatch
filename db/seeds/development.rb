bf46aa1143a9579b893e585c364ef807b36518c3
Jessica G <gillan.jessica@gmail.com>
2017-02-13 12:34:01 -0700

    dont erase DB when seed

--- db/seeds/development.rb
+++ db/seeds/development.rb
@@ -1,14 +1,7 @@
 
-puts "Deleting database"
-
-Watchable.destroy_all
 
 puts "Populating all Movie titles from movie_api"
 
 Movie.populate_db_titles
-
-puts "Add url info to all Movie titles"
-
-Movie.populate_watchables_data
 
 puts "Done"
