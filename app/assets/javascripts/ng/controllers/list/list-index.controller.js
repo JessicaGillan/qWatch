fdf1d08d9be6408e30aec4712949e55a028b231e
Sampson Crowley <sampsonsprojects@gmail.com>
2017-02-14 21:33:18 -0700

    functioning infinite scroll

--- app/assets/javascripts/ng/controllers/list/list-index.controller.js
+++ app/assets/javascripts/ng/controllers/list/list-index.controller.js
@@ -4,78 +4,103 @@ qWatch.controller('ListIndexCtrl',[
 
     var el = document.getElementById('watchable-search'),
         rowHeight = 530,
-        scroll = 0;
-        console.log(el)
+        scroll = 0,
+        angWindow = angular.element($window);
 
     $scope.currentItem = {};
 
     $scope.offset = {
       new: 0,
       last: 0,
-      begin: 0
+      begin: 0,
+      firstEl: angular.element(el).offset().top
+    }
+    console.log($scope.offset)
+
+    var _resetOffset = function _resetOffset(){
+      angular.copy({
+        new: 0,
+        last: 0,
+        begin: 0
+      }, $scope.offset)
     }
 
-    // var _resetOffset = function _resetOffset(){
-    //   angular.copy({
-    //     new: 0,
-    //     last: 0,
-    //     begin: 0
-    //   }, $scope.offset)
-    // }
-    //
-    // var _calcFirst = function _calcFirst(scroll){
-    //   return Math.floor(($scope.offset.new/rowHeight));
-    // }
-    //
-    // var delayedExec = function(after, fn) {
-    //     var timer;
-    //     return function() {
-    //         timer && $timeout.cancel(timer);
-    //         timer = $timeout(fn, after);
-    //     };
-    // };
-    //
-    // var _debouncer = delayedExec(100, function() {
-    //     if($scope.offset.new > $scope.offset.last){
-    //       var newOffset = _calcFirst() > 50 ? Math.floor(_calcFirst()/25) - 1 : 0;
-    //       $scope.offset.begin += newOffset * 25;
-    //       el.scrollTop -= rowHeight * newOffset * 25;
-    //     } else if($scope.offset.new < $scope.offset.last) {
-    //       var newOffset = _calcFirst() < 50 ? 25 : 0;
-    //       if($scope.offset.begin <= newOffset){
-    //         el.scrollTop += rowHeight * $scope.offset.begin;
-    //         $scope.offset.begin = 0
-    //       } else{
-    //         $scope.offset.begin -= newOffset;
-    //         el.scrollTop += rowHeight * newOffset;
-    //       }
-    //     }
-    //     $scope.offset.last = el.scrollTop;
-    // });
+    var _calcFirst = function _calcFirst(scroll){
+      return Math.floor(($scope.offset.new/rowHeight));
+    }
+
+    var delayedExec = function(after, fn) {
+      var timer;
+      return function() {
+          timer && $timeout.cancel(timer);
+          timer = $timeout(fn, after);
+      };
+    };
+
+    var _checkScrollDown = function _checkScrollDown(){
+      return _calcFirst() >= 15;
+    }
+
+    var _scrollDown = function _scrollUp(bypass) {
+      if(bypass || _checkScrollDown()){
+        angWindow.scrollTop(angWindow.scrollTop() - (5 * rowHeight));
+        $scope.offset.begin += 15;
+        if(Math.floor(($scope.list.length - $scope.offset.begin)/3) - _calcFirst() < 5){
+          watchable.index(true);
+        }
+        $timeout(function(){
+          if(_checkScrollDown()) _scrollDown(true);
+        })
+      }
+    }
+
+    var _checkScrollUp = function _checkScrollUp(){
+      return _calcFirst() <= 10 && $scope.offset.begin !== 0
+    }
+
+    var _scrollUp = function _scrollUp(bypass) {
+      if(bypass || _checkScrollUp()){
+        angWindow.scrollTop(angWindow.scrollTop() + (5 * rowHeight));
+        $scope.offset.begin -= 15;
+        $timeout(function(){
+          if(_checkScrollUp()) _scrollUp(true);
+        })
+      }
+    }
+
+    var _debouncer = delayedExec(100, function() {
+      if($scope.offset.new > $scope.offset.last){
+        _scrollDown();
+      } else if($scope.offset.new < $scope.offset.last) {
+        _scrollUp();
+      }
+      $scope.offset.last = angWindow.scrollTop() - $scope.offset.firstEl;
+    });
 
     $scope.$watch('currentItem.id', function(){
       $timeout(function(){
-        scroll = angular.element($window).scrollTop();
+        scroll = angWindow.scrollTop();
       });
     })
 
     angular.element(document).on('scroll', function (e) {
       if($scope.currentItem.id){
-        angular.element($window).scrollTop(scroll);
+        angWindow.scrollTop(scroll);
+      } else {
+        $scope.offset.new = angWindow.scrollTop() - $scope.offset.firstEl;
+        _debouncer()
       }
-      // $scope.offset.new = e.target.scrollTop;
-      // _debouncer()
     });
 
-    $scope.next = function next(){
-      watchable.index(true);
-      $scope.offset.begin += 100;
-    }
-
-    $scope.previous = function previous(){
-      watchable.index(true);
-      $scope.offset.begin -= 100;
-    }
+    // $scope.next = function next(){
+    //   watchable.index(true);
+    //   // $scope.offset.begin += 100;
+    // }
+    //
+    // $scope.previous = function previous(){
+    //   watchable.index(true);
+    //   // $scope.offset.begin -= 100;
+    // }
 
 
     var setToIndex = function setToIndex(){
--- app/assets/javascripts/ng/controllers/search.controller.js
+++ app/assets/javascripts/ng/controllers/search.controller.js
@@ -1,7 +1,12 @@
 qWatch.controller('SearchCtrl',[
-  '$scope', '$rootScope', '$timeout',
-  function($scope, $rootScope, $timeout){
-    var _handler, searchBar;
+  '$scope', '$rootScope', '$timeout', '$window',
+  function($scope, $rootScope, $timeout, $window){
+    var _handler, searchBar,
+        searchEl = angular.element('#search-panel'),
+        searchElFiller = angular.element('#search-panel-filler'),
+        rect = searchEl.get(0).getBoundingClientRect(),
+        offset = searchEl.offset().top - rect.height,
+        showing = false;
 
     $scope.search = {term: ""};
 
@@ -17,35 +22,61 @@ qWatch.controller('SearchCtrl',[
       }, 300)
     };
 
+    var _resetSearchEl = function _resetSearchEl(){
+      $scope.showFiller = false
+      searchEl.css({position: "", top: "", width: ""})
+    }
+
+    var _checkSearchElPosition = function _checkSearchElPosition(){
+      return (angular.element($window).scrollTop() >= offset - 60 && !showing)
+    }
+
+    var _setSearchElFixed = function _setSearchElFixed(){
+      searchEl.css({transitionDuration: "0s", position: "fixed", top: "51px", width: rect.width})
+      $scope.showFiller = true;
+    }
+
     var _slideUp = function _slideUp(){
       _slideDown();
+      showing = true;
+      _resetSearchEl();
       angular.element('#header-wrapper').css({"max-height": 0, padding: 0});
     }
 
     var _slideUpFixed = function _slideUpFixed(){
       _slideUp();
       searchBar = searchBar || angular.element('.title-search');
-      var rect = searchBar.get(0).getBoundingClientRect();
-      searchBar.css({position: "fixed", top: rect.top, width: rect.width})
+      var srect = searchBar.get(0).getBoundingClientRect();
+
+      searchBar.css({position: "fixed", top: srect.top, width: srect.width})
       $timeout(function(){
         searchBar.css({top: "51px"})
       })
     }
 
     var _slideDown = function _slideDown(){
+      showing = false;
       angular.element('#header-wrapper').css({"max-height": "", padding: ""});
       searchBar = searchBar || angular.element('.title-search');
       searchBar.css({position: "", left: "", top: "", width: ""})
+      if(_checkSearchElPosition()) _setSearchElFixed();
     }
 
     $scope.$watch('search.term', searchFor)
 
-    // When search bar is no longer pristine, remove title/logo and
-    // slide form upwards.
     $rootScope.$on('searchSet', _slideUp);
     $rootScope.$on('showItem', _slideUpFixed);
 
     $rootScope.$on('searchClear', _slideDown);
     $rootScope.$on('hideItem', _slideDown);
+
+    angular.element(document).on('scroll', function (e) {
+
+      if(_checkSearchElPosition()){
+        _setSearchElFixed();
+      } else {
+        _resetSearchEl();
+      }
+    });
   }
 ])
