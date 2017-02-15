a1444d94e119adc6859fcace09cfe3b2e2a87b62
Jessica G <gillan.jessica@gmail.com>
2017-02-15 10:45:18 -0700

    add and style share buttons directive

---
+++ app/assets/javascripts/ng/directives/share-buttons.directive.js
@@ -0,0 +1,17 @@
+qWatch.directive('shareButtons', function() {
+
+  return {
+    restrict: 'E',
+    scope: {
+      title: '@',
+      shareUrl: '@',
+      shareImg: '@'
+    },
+    templateUrl: 'ng/directives/share-buttons.html',
+    link: function(scope, element, attrs) {
+      // TODO: change || setting to location.href
+      // FB will not owrk with localhost urls
+      scope.shareUrl = scope.shareUrl || "https://qwatch.me/"
+    }
+  };
+});
