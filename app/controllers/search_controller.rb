d466992477f89698dc8a432d3d4dee62af531561
Sampson Crowley <sampsonsprojects@gmail.com>
2017-02-22 14:59:06 -0700

    comments

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,15 @@
+# Public: API controller for searching for a given title
+#
 class SearchController < ApplicationController
+
   def index
+    # get the title to search for
     terms = params[:search]
+
+    # if the strict param is set, only return exact matches
     strict = params[:strict] != "false" ? true : false
+
+    # grab all matching titles
     render json: Watchable.title_search(terms, strict)
   end
 end
